0d0f6d6837b86fafca91df8c9e1c66a98ac5c34c
Tooba Abbassi-Daloii <t.abbassidaloii@maastrichtuniversity.nl>
2023-08-17 15:50:26 +0200

    NCBI data processing

---
+++ scripts/NCBI_processing.R
@@ -0,0 +1,24 @@
+# Clear environment and load necessary libraries
+rm(list = ls())
+require(downloader)
+library(dplyr)
+
+# Set variables
+sourceName <- "NCBI"
+sourceVersion <- "2023-08-14"
+inputDir <- "mapping_preprocessing/datasources"
+
+# Create output directory
+outputDir <- "IDRefiner/Docker/app/processed_mapping_files"
+dir.create(outputDir, showWarnings = FALSE)
+
+## Download the input files from NCBI (better to download and make a subset for human and mice in bash)
+# fileUrl <- "https://ftp.ncbi.nih.gov/gene/DATA/gene_history.gz"
+# download(fileUrl, paste(inputDir, sourceName, "gene_history.gz", sep = "/"), mode = "wb")
+# 
+# fileUrl <- "https://ftp.ncbi.nih.gov/gene/DATA/gene_info.gz"
+# download(fileUrl, paste(inputDir, sourceName, "gene_info.gz", sep = "/"), mode = "wb")
+
+# Read the file that includes the withdrawn ids
+
+##NCBI_WDN <- read.csv(paste(inputDir, sourceName, paste0("gene_history_human_mice_", sourceVersion), sep = "/"), sep = "\t") %>% 
