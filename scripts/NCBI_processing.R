706a2aa7cf82c4006d316bde5bbfe87b69fc62ae
Tooba Abbassi-Daloii <31986271+tabbassidaloii@users.noreply.github.com>
2023-08-28 10:30:54 +0200

    Update NCBI_processing.R

--- scripts/NCBI_processing.R
+++ scripts/NCBI_processing.R
@@ -1,6 +1,8 @@
 # Clear environment and load necessary libraries
 rm(list = ls())
+if(!"readr" %in% installed.packages()){install.packages("downloader")}
 require(downloader)
+if(!"readr" %in% installed.packages()){install.packages("dplyr")}
 library(dplyr)
 
 # Set variables
