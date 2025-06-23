44d94c1ca52a6a31fad22c3b173eb1179c75365b
Tooba Abbassi-Daloii <t.abbassidaloii@maastrichtuniversity.nl>
2025-06-23 11:55:44 +0200

    R to java
    Implemented until
    # Read the file that includes the gene info

--- java/src/org/sec2pri/ncbi_txt.java
+++ java/src/org/sec2pri/ncbi_txt.java
@@ -7,49 +7,194 @@ import java.io.*;
 import java.sql.SQLException;
 import java.text.SimpleDateFormat;
 import java.util.*;
+import java.util.zip.GZIPInputStream;
 
-public class ncbi_gene {
+/**
+ * Mapping the secondary identifiers (retired or withdrawn identifies) to primary identifiers (currently used identifiers).
+ * 
+ * @author tabbassidaloii
+ */
+
+public class ncbi_txt {
     public static String sourceName = "NCBI";
     public static String sourceIdCode = "L"; // GenBank/RefSeq
     public static String sourceSynonymCode = "O"; // Gene names as synonyms
     public static String DbVersion = "1";
     public static String BridgeDbVersion = "3.0.10";
-
     private static DataSource dsId;
     private static DataSource dsName;
     private static GdbConstruct newDb;
 
     public static void main(String[] args) throws IOException, IDMapperException, SQLException {
         try {
-            File gene2accessionFile = new File(args[0]);
-            File geneInfoFile = new File(args[1]);
-            File outputDir = new File(args[2]);
+    		//Assign the input argument to the corresponding variables
+        	String sourceVersion = args[0];
+        	File geneHistoryFile = new File(args[1]);
+            File geneInfoFile = new File(args[2]);
+            setupDatasources();
+            System.out.println("[INFO]: args[0] = " + args[0]);
+            System.out.println("[INFO]: args[1] = " + args[1]);
+            System.out.println("[INFO]: args[2] = " + args[2]);
+            System.out.println("[INFO]: sourceName = " + sourceName);
+            File outputDir = new File("datasources/", sourceName.toLowerCase() + "/recentData");;
+            System.out.println("[INFO]: outputDir = " + outputDir);
             outputDir.mkdir();
 
-            setupDatasources();
+            //Create output bridge mapping file
+            File outputFile = new File(outputDir, sourceName + "_secID2priID.bridge");
+            createDb(outputFile);
 
-            File bridgeFile = new File(outputDir, sourceName + "_secID2priID.bridge");
-            createDb(bridgeFile);
-
+    		//Create output tsv mapping files
+    		////primary ID
+    		List<List<String>> listOfpri = new ArrayList<>(); //list of primary IDs
+    		////Secondary to primary ID
+    		List<List<String>> listOfsec2pri = new ArrayList<>(); //list of the secondary to primary IDs
+    		////symbol 2 alias & previous names
+    		List<List<String>> listOfsymbol2alias = new ArrayList<>(); //list of the name to synonym 
+            
             Map<Xref, Set<Xref>> map = new HashMap<>();
+            
+            //Read the file that includes the withdrawn ids
+            BufferedReader accReader = new BufferedReader(
+            	    new InputStreamReader(new GZIPInputStream(new FileInputStream(geneHistoryFile)))
+            	);
+            String line = accReader.readLine(); // read header
+            Map<String, Integer> colIdx = new HashMap<>();
+            String[] headers = line.split("\t");
 
-            BufferedReader accReader = new BufferedReader(new FileReader(gene2accessionFile));
-            String line;
+            // Store column indexes
+            for (int i = 0; i < headers.length; i++) {
+                colIdx.put(headers[i], i);
+            }
+
             while ((line = accReader.readLine()) != null) {
-                if (line.startsWith("#") || line.trim().isEmpty()) continue;
+                if (line.trim().isEmpty()) continue;
 
                 String[] fields = line.split("\t", -1);
-                String geneId = fields[1];
-                String rnaAcc = fields[3];
-                String protAcc = fields[5];
 
-                Xref geneX = new Xref(geneId, dsId);
-                map.putIfAbsent(geneX, new HashSet<>());
+                String taxId = fields[colIdx.get("#tax_id")];
+                if (!taxId.equals("9606")) continue; // human genes only
 
-                if (!rnaAcc.equals("-")) map.get(geneX).add(new Xref(rnaAcc, dsId, false));
-                if (!protAcc.equals("-")) map.get(geneX).add(new Xref(protAcc, dsId, false));
+                String primaryID = fields[colIdx.get("GeneID")];
+                String secondaryID = fields[colIdx.get("Discontinued_GeneID")];
+                String secondarySymbol = fields[colIdx.get("Discontinued_Symbol")];
+                String comment = fields[colIdx.get("Discontinue_Date")];
+
+                // Handle missing primaryID
+                if (primaryID.equals("-")) {
+                    primaryID = "Entry Withdrawn";
+                }
+
+                // Format comment
+                if (!comment.equals("-")) {
+                    comment = "Withdrawn date: " + comment + ". ";
+                } else {
+                    comment = "";
+                }
+
+                List<String> row = Arrays.asList(primaryID, secondaryID, secondarySymbol, comment);
+                listOfsec2pri.add(row);
             }
             accReader.close();
+            
+            System.out.println("[INFO]: listOfsec2pri = " + listOfsec2pri);
+
+			//Since the data for NCBI is coming form two files, it would be more accurate to add the required information for SSSOM format while prepossessing the files
+			//Add the proper predicate: 
+			////IAO:0100001 for IDs merged or 1:1 replacement to one and 
+			////oboInOwl:consider for IDs split or deprecated/withdrawn
+            Map<String, Integer> primaryCount = new HashMap<>();
+            Map<String, Integer> secondaryCount = new HashMap<>();
+
+            for (List<String> row : listOfsec2pri) {
+                String primaryID = row.get(0);
+                String secondaryID = row.get(1);
+
+                primaryCount.put(primaryID, primaryCount.getOrDefault(primaryID, 0) + 1);
+                secondaryCount.put(secondaryID, secondaryCount.getOrDefault(secondaryID, 0) + 1);
+            }
+
+            List<List<String>> finalMappedList = new ArrayList<>();
+
+            Set<String> allSecondaryIDs = new HashSet<>();
+            for (List<String> row : listOfsec2pri) {
+                allSecondaryIDs.add(row.get(1)); // secondaryID
+            }
+            
+            for (List<String> row : listOfsec2pri) {
+                String primaryID = row.get(0);
+                String secondaryID = row.get(1);
+                String secondarySymbol = row.get(2);
+                String comment = row.get(3);
+
+                Integer priCount = primaryCount.get(primaryID);
+                Integer secCount = secondaryCount.get(secondaryID);
+
+                String cardinality;
+                if (primaryID.equals("Entry Withdrawn")) {
+                    cardinality = "1:0";
+                    priCount = null;
+                } else if (priCount > 1 && secCount == 1) {
+                    cardinality = "n:1";
+                } else if (priCount == 1 && secCount == 1) {
+                    cardinality = "1:1";
+                } else if (priCount == 1 && secCount > 1) {
+                    cardinality = "1:n";
+                } else if (priCount > 1 && secCount > 1) {
+                    cardinality = "n:n";
+                } else {
+                    cardinality = null;
+                }
+
+                // Predicate
+                String predicateID;
+                if (Arrays.asList("1:0", "1:n", "n:n").contains(cardinality)) {
+                    predicateID = "oboInOwl:consider";
+                } else if (Arrays.asList("1:1", "n:1").contains(cardinality)) {
+                    predicateID = "IAO:0100001";
+                } else {
+                    predicateID = null;
+                }
+
+                // Enhanced comment
+             // Enhanced comment
+                if (cardinality != null) {
+                    switch (cardinality) {
+                        case "1:n":
+                            comment += "ID (subject) is split into multiple.";
+                            break;
+                        case "1:1":
+                            comment += "ID (subject) is replaced.";
+                            break;
+                        case "n:n":
+                            comment += "This ID (subject) and other ID(s) are merged/split into multiple ID(s).";
+                            break;
+                        case "n:1":
+                            comment += "This ID (subject) and other ID(s) are merged into one ID.";
+                            break;
+                        case "1:0":
+                            comment += "ID (subject) withdrawn/deprecated.";
+                            break;
+                    }
+                }
+
+                // Additional note if primaryID is itself withdrawn
+                if (allSecondaryIDs.contains(primaryID)) {
+                    comment += " Object is also withdrawn.";
+                }
+
+                // Append release info
+                comment += " Release: " + sourceVersion + ".";
+
+                String source = "https://ftp.ncbi.nih.gov/gene/DATA/gene_history.gz";
+
+                List<String> newRow = Arrays.asList(primaryID, secondaryID, secondarySymbol,
+                                                    predicateID, cardinality, comment, source);
+                finalMappedList.add(newRow);
+            }
+            System.out.println("[INFO]: finalMappedList = " + finalMappedList);
+
+            
 
             BufferedReader infoReader = new BufferedReader(new FileReader(geneInfoFile));
             while ((line = infoReader.readLine()) != null) {
