a796e37b04d39ef46bceec5e2461cfa738ddd99f
Tooba Abbassi-Daloii <t.abbassidaloii@maastrichtuniversity.nl>
2025-07-16 12:05:51 +0200

    Revert "Update uniprot.R"
    This reverts commit 8ceb351185dc2208896e57115c83d3a03846f914.

--- r/src/uniprot.R
+++ r/src/uniprot.R
@@ -28,7 +28,7 @@ if(!"R.utils" %in% installed.packages()) {
 args <- commandArgs(trailingOnly = TRUE)
 
 # Set variables
-sourceName = "uniprot"
+sourceName = "UniProt"
 sourceVersion = args[1]
 uniprot_sprot = args[2]
 delac_sp = args[3] 
@@ -36,7 +36,7 @@ sec_ac = args[4]
 inputDir <- "datasources"
 
 # Create output directory
-outputDir <- paste0("datasources/", sourceName, "/recentData")
+outputDir <- paste0("datasources/", tolower(sourceName), "/recentData")
 dir.create(outputDir, showWarnings = FALSE)
 
 # Download the input files from UniProt
